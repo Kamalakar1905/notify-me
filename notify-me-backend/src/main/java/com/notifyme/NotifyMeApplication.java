31592e3709e4d4046260e1936ede70b00260a307
Kamalakar1905 <132550496+Kamalakar1905@users.noreply.github.com>
2026-05-25 17:07:22 +0530

    Minor changes

--- notify-me-backend/src/main/java/com/notifyme/NotifyMeApplication.java
+++ notify-me-backend/src/main/java/com/notifyme/NotifyMeApplication.java
@@ -11,5 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NotifyMeApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotifyMeApplication.class, args);
+        System.out.println("Running in Dev");
     }
 }
